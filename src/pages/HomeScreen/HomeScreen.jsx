aec03642d4ed9db128cc3a756f9e35940211cc68
huuquangg <21522508@gm.uit.edu.vn>
2024-11-03 12:30:13 +0700

    THES-17: Add UI for HomeScreen.

--- src/pages/HomeScreen/HomeScreen.jsx
+++ src/pages/HomeScreen/HomeScreen.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Slide from "../../components/Slide/Slide";
+import HomeFeaturesTable from "./HomeFeatures";
+import HomeBenifit from "./HomeBenifits";
+import HomeShowCases from "./ShowCases/HomeShowCase";
+
 
 const Homepage = () => {
-    return (
-        <div className="homepage-container">
-            <Navbar />
-            <Slide />
-        </div>
-    );
-}
+  return (
+    <div className="homepage-container">
+      <Navbar />
+      <Slide />
+      <HomeFeaturesTable />
+      <HomeBenifit />
+      <HomeShowCases />
+    </div>
+  );
+};
 
 export default Homepage;
