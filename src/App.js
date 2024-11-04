3f1dc01a285011202ea826d465beb9c98c094a79
huuquangg <21522508@gm.uit.edu.vn>
2024-11-04 13:51:54 +0700

    THES-17: Refactor Admin dashboard and add some pages.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Home from "./pages/HomeScreen/HomeScreen";
 
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-import TaskDashboard from "./pages/ManagerDashboard/TaskManagerDashboard";
 import AdminPage from "./pages/Administration/AdminPage";
 
 import ComicForm from "./components/comic/ComicForm/ComicForm";
+import ComicPage from "./pages/ComicManagement/ComicPage";
 import CreateComic from "./pages/ComicManagement/CreateComic";
 import EditComic from "./pages/ComicManagement/EditComic";
 import ComicInfo from "./pages/ComicManagement/ComicInfo";
@@ -20,6 +20,8 @@ import TaskList from "./pages/TaskManagement/TaskList";
 import AddTask from "./pages/TaskManagement/CreateTask";
 import EditTask from "./pages/TaskManagement/EditTask";
 
+import PublishingPage from "./pages/PublishingManagement/PublishingPage";
+
 import UserManagement from "./pages/UserManagement/UserManagement";
 import AddUser from "./pages/UserManagement/CreateUser";
 import EditUser from "./pages/UserManagement/EditUser";
@@ -28,18 +30,18 @@ import UserInfo from "./pages/UserManagement/UserInfo";
 import Unauthorized from "./pages/Error/Unauthorized";
 
 function App() {
-  <link href="./output.css" rel="stylesheet"></link>
+  <link href="./output.css" rel="stylesheet"></link>;
   return (
     <Router>
       <Routes>
         {/* Login and sign up */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<TaskDashboard />} />
         {/* Home and admin pages */}
         <Route path="/home" element={<Home />} />
         <Route path="/admin/dashboard" element={<AdminPage />} />
         {/* Layout and dashboard */}
+        <Route path="admin/comicsmanage" element={<ComicPage />} />
         <Route path="/comics" element={<ComicForm />} />
         <Route path="/admin/addcomic" element={<CreateComic />} />
         <Route path="/admin/editcomic" element={<EditComic />} />
@@ -49,6 +51,11 @@ function App() {
         <Route path="/admin/tasklist" element={<TaskList />} />
         <Route path="/admin/addtask" element={<AddTask />} />
         <Route path="/admin/edittask" element={<EditTask />} />
+
+        {/*Task management*/}
+        <Route path="/admin/publishing" element={<PublishingPage />} />
+        {/* <Route path="/admin/tasklist" element={<TaskList />} /> */}
+
         {/*User management*/}
         <Route path="/admin/usermanage" element={<UserManagement />} />
         <Route path="/admin/adduser" element={<AddUser />} />
@@ -57,7 +64,6 @@ function App() {
         <Route path="/admin/userinfo" element={<UserInfo />} />
         {/*Exception Pages*/}
         <Route path="/unauthorized" element={<Unauthorized />} />
-
       </Routes>
     </Router>
   );
---
+++ src/pages/PublishingManagement/PublishingPage.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Assumes you're using react-router
+import Layout from "../PublishingManagement/PublishingLayout";
+import { jwtDecode } from "jwt-decode";
+
+function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if 'authKey' exists in session storage
+    const authToken = sessionStorage.getItem("authToken");
+
+    if (!authToken) {
+      // Redirect to login page if authToken is missing
+      navigate("/login");
+    } else {
+      try {
+        // Decode the authToken to get the role
+        const decodedToken = jwtDecode(authToken);
+        const userRole =
+          decodedToken[
+            "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+          ];
+
+        console.log(decodedToken);
+        console.log(userRole);
+
+        // // Check if user has an admin role
+        if (
+          userRole !== "SuperAdmin" &&
+          userRole !== "Quality Control" &&
+          userRole !== "Manager" &&
+          userRole !== "Employee"
+        ) {
+          // Redirect non-admin users to a different page
+          navigate("/unauthorized");
+        }
+      } catch (error) {
+        // Handle decoding errors (e.g., if authToken is invalid)
+        console.error("Invalid authToken:", error);
+        navigate("/login");
+      }
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
