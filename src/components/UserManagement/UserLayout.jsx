f0916231cbd1549680696a467498bf884a567ede
S H 7 D 0 W <sh7d0w.atyourservice@gmail.com>
2024-11-06 14:47:09 +0700

    THES-17: Update task management table and add user management component

--- src/components/UserManagement/UserLayout.jsx
+++ src/components/UserManagement/UserLayout.jsx
@@ -3,16 +3,24 @@ import React from "react";
 import Sidebar from "../Admin/AdminSidebar";
 import Header from "../Admin/AdminHeader";
 import Dashboard from "./UserDashboard";
+import TaskDashboard from "../../pages/TaskManagement/TaskManagerDashboard";
 
 export default function Layout() {
     return (
-        <div className="flex flex-row  bg-neutral-100 h-screen w-screen
-        overflow-hidden">
-            <Sidebar />
-            <div className="flex-1">
-                <Header />
-                <Dashboard />
-            </div>
+        <div className="flex bg-neutral-100 h-screen w-screen">
+        {/* Sidebar */}
+        <div className="flex bg-neutral-200">
+          <Sidebar />
         </div>
+        
+        {/* Main content */}
+        <div className="flex-1 flex flex-col overflow-y-scroll">
+          <Header />
+          <div className="p-2"> {/* Reduced padding here */}
+            <Dashboard />
+            <TaskDashboard />
+          </div>
+        </div>
+      </div>
     );
 }
